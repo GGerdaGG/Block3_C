41e06e3883de10f3f65bab3b1486fc88f21e1a1b
Kate <suhoste1@gmail.com>
2022-11-17 12:03:43 +0800

    add task08-25

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 456 -> 6 782 -> 2 918 -> 8
+
+int num = 456; // 456/10 = 45 / 10 = 4
+int last = num % 10; // 6
+int first = num / 100; // 4
