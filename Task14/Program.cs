efe68c63f0ab4e7173a8660a078b2351bee29636
Kate <suhoste1@gmail.com>
2022-11-17 12:09:57 +0800

    Добавила условия задач в папки

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
